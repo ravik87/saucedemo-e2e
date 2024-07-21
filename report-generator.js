a887664864ca9831e460d9c98c25729c333f0460
Ravi <ravi.kandpal87@gmail.com>
2024-07-21 19:38:18 +0530

    feat: Add cucumber reporting

---
+++ report-generator.js
@@ -0,0 +1,24 @@
+const report = require('multiple-cucumber-html-reporter')
+const path = require("path");
+const projectName = path.basename(__dirname);
+const projectVersion = process.env.npm_package_version;
+const reportGenerationTime = new Date().toISOString();
+
+report.generate({
+    reportName: 'E2E Test Report',
+    jsonDir: 'reports/cucumber-json-reports',
+    reportPath: 'reports/cucumber-json-reports/html',
+    openReportInBrowser: true,
+    disableLog: true,
+    displayDuration: true,
+    displayReportTime: true,
+    durationInMS: true,
+    customData: {
+        title: 'Run info',
+        data: [
+            { label: 'Project', value: `${projectName}` },
+            { label: 'Release', value: `${projectVersion}` },
+            { label: 'Report Generation Time', value: `${reportGenerationTime}` },
+        ],
+    },
+});
