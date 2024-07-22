99f9a06aeb1cd9c4360e973134756c9a6d5011f3
Ravi <ravi.kandpal87@gmail.com>
2024-07-22 12:52:24 +0530

    refactor: move ui messages to enum

--- cypress/support/assertion/web_ui/checkout_complete_page_assert.js
+++ cypress/support/assertion/web_ui/checkout_complete_page_assert.js
@@ -1,13 +1,13 @@
 import SwagLabs from "../../modules/web_ui/swag_labs";
+import UIContent from "../../general/enums/ui_contents";
 
 class CheckoutCompletePageAssert {
 
     checkOrderConfirmation() {
         cy.get(SwagLabs.checkoutCompletePage().completeHeader())
-            .should('have.text', 'Thank you for your order!');
+            .should('have.text', UIContent.CHECKOUT_COMPLETE_MSG);
         cy.get(SwagLabs.checkoutCompletePage().completeText())
-            .should('have.text', 'Your order has been dispatched, ' +
-                'and will arrive just as fast as the pony can get there!');
+            .should('have.text', UIContent.CHECKOUT_COMPLETE_INFO);
         return this;
     }
 
---
+++ cypress/support/general/enums/ui_contents.js
@@ -0,0 +1,9 @@
+const UIContent = {
+    CHECKOUT_COMPLETE_MSG: 'Thank you for your order!',
+    CHECKOUT_COMPLETE_INFO: 'Your order has been dispatched, ' +
+        'and will arrive just as fast as the pony can get there!'
+};
+
+Object.freeze(UIContent);
+
+export default UIContent
